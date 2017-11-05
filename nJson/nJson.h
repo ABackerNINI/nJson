7212729af779e5f61a74d6003510f95b76f5b6e0
ABackerNINI <abacker.nini@foxmail.com>
2017-11-05 16:43:04 +0800

    add comments

--- nJson/nJson.h
+++ nJson/nJson.h
@@ -20,14 +20,25 @@
 #include "support/cstr_type.h"
 #include "support/string_type.h"
 
+/* Template types support files.
+ * You need to rearrange the include order of these files to support some special nesting types.
+ * For example,include "list_type.h" and then include "map_type.h",
+ * the following types are supported:
+ *      list<T>,
+ *      map<T1,T2>,
+ *      list<list<T>>,
+ *      map<T1,list<T2>>,
+ *      map<T1,list<T2>>,
+ *      and some other nesting types exclude "list<map<T1,T2>>".
+ * To support "list<map<T1,T2>>",include "list_type.h" after "map_type.h".
+ * */
 #include "support/list_type.h"
-#include "support/pointer_type.h"
 #include "support/map_type.h"
 #include "support/userdef_type.h"
-
 /*
 	Includes "support/pointer_type.h" after other type-support files to support their corresponding pointer types.
 	So how to support the pointer types of other types? I haven't figured out.
 */
+#include "support/pointer_type.h"
 
 #endif//_NJSON_NJSON_H_
