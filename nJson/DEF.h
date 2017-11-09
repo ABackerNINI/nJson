3fdcaa5fef8046b3ed349f18f69873c011d716ca
ABacker <1105908317@qq.com>
2017-11-09 21:05:15 +0800

    re-define definations

--- nJson/DEF.h
+++ nJson/DEF.h
@@ -5,13 +5,16 @@
 
 #include "nJson.h"
 
-#define SET(M) _SET(njson_val,M)
-#define GET(M) _GET(njson_val,M)
-#define SET_IF_KEY(M) if(strcmp(#M,_key_) == 0) { SET(M); } else
-#define SERIALIZE_SUPER_CLASS(S) _SERIALIZE_SUPER_CLASS((S *)this,njson_val)
-#define DESERIALIZE_SUPER_CLASS(S) _DESERIALIZE_SUPER_CLASS((S *)this,njson_val)
+#define NJSON_SET(M) _NJSON_SET(njson_val,#M,M)
+#define NJSON_SET_AS_TYPE(M, T) _NJSON_SET(njson_val,#M,(T)(M))
+#define NJSON_GET(M) _NJSON_GET(njson_val,#M,&(M))
+#define NJSON_GET_AS_TYPE(M, T) _NJSON_GET(njson_val,#M,(T *)(&(M)))
 
-#define SET_IF(K, M) _SET_IF_NOT_DEFAULT(_root_obj_,K,M)
+#define NJSON_SET_IF_KEY(M) if(strcmp(#M,_key_) == 0) { NJSON_SET(M); } else
+#define NJSON_SERIALIZE_SUPER_CLASS(S) _NJSON_SERIALIZE_SUPER_CLASS((S *)this,njson_val)
+#define NJSON_DESERIALIZE_SUPER_CLASS(S) _NJSON_DESERIALIZE_SUPER_CLASS((S *)this,njson_val)
+
+#define SET_IF(K, M) _NJSON_SET_IF_NOT_DEFAULT(_root_obj_,K,M)
 
 inline int cut_string(char *cstr, char c);
 
@@ -40,7 +43,7 @@ inline int cut_string(char *cstr, char c);
 
 /*Return c-style string,free it afterwards*/
 template<typename _T>
-inline char *serialize(const _T &res) {
+inline char *njson_serialize(const _T &res) {
     JSON_Value *doc = json_value_init_object();
 
     njson_serialize(doc, NULL, res);
@@ -53,7 +56,7 @@ inline char *serialize(const _T &res) {
 }
 
 template<typename T>
-inline void deserialize(T *res, const char *s) {
+inline void njson_deserialize(T *res, const char *s) {
     JSON_Value *doc = json_parse_string(s);
 
     njson_deserialize(doc, res);
--- nJson/nJson.h
+++ nJson/nJson.h
@@ -3,19 +3,19 @@
 #ifndef _NJSON_NJSON_H_
 #define _NJSON_NJSON_H_
 
-#define _IS_DEFAULT_VALUE ::njson_is_default_value
-#define _SET_VALUE ::njson_serialize
-#define _GET_VALUE ::njson_deserialize
+#define _NJSON_IS_DEFAULT_VALUE ::njson_is_default_value
+#define _NJSON_SET_VALUE ::njson_serialize
+#define _NJSON_GET_VALUE ::njson_deserialize
 
-#define _SET_IF_NOT_DEFAULT(ROOT, K, M) if(!_IS_DEFAULT_VALUE(M))_SET_VALUE(ROOT,K,M)
+#define _NJSON_SET_IF_NOT_DEFAULT(ROOT, K, M) if(!_NJSON_IS_DEFAULT_VALUE(M))_NJSON_SET_VALUE(ROOT,K,M)
 
-#define _SET(ROOT, M) _SET_IF_NOT_DEFAULT(ROOT,#M,M)
+#define _NJSON_SET(ROOT,K, M) _NJSON_SET_IF_NOT_DEFAULT(ROOT,K,M)
 
-#define _GET(ROOT, M) if(json_object_has_value(json_value_get_object(ROOT),#M)) \
-    _GET_VALUE(json_object_get_value(json_value_get_object(ROOT),#M),&(M))
+#define _NJSON_GET(ROOT,K, M) if(json_object_has_value(json_value_get_object(ROOT),K)) \
+    _NJSON_GET_VALUE(json_object_get_value(json_value_get_object(ROOT),K),M)
 
-#define _SERIALIZE_SUPER_CLASS(SUPER, ROOT) _SET_VALUE(ROOT,NULL,*(SUPER))
-#define _DESERIALIZE_SUPER_CLASS(SUPER, ROOT) _GET_VALUE(ROOT,SUPER)
+#define _NJSON_SERIALIZE_SUPER_CLASS(SUPER, ROOT) _NJSON_SET_VALUE(ROOT,NULL,*(SUPER))
+#define _NJSON_DESERIALIZE_SUPER_CLASS(SUPER, ROOT) _NJSON_GET_VALUE(ROOT,SUPER)
 
 #include "support/support_base.h"
 #include "support/basic_type.h"
@@ -26,19 +26,5 @@
 #include "support/vector_type.h"
 #include "support/map_type.h"
 #include "support/pointer_type.h"
-
-/* Template types support files.
- * You need to rearrange the include order of these files to support some special nesting types.
- * For example,include "list_type.h" and then include "map_type.h",
- * the following types are supported:
- *      list<T>,
- *      map<T1,T2>,
- *      list<list<T>>,
- *      map<T1,list<T2>>,
- *      map<T1,list<T2>>,
- *      and some other nesting types exclude "list<map<T1,T2>>".
- * To support "list<map<T1,T2>>",include "list_type.h" after "map_type.h".
- * */
-
 
 #endif//_NJSON_NJSON_H_
