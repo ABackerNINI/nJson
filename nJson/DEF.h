a66b76125c96afc2534db99f017e5098a00bd054
ABackerNINI <abacker.nini@foxmail.com>
2017-11-19 21:09:46 +0800

    define NJSON_VAL,NJSON_VAR,NJSON_TMP_VAL,NJSON_RET

--- nJson/DEF.h
+++ nJson/DEF.h
@@ -5,24 +5,26 @@
 
 #include "nJson.h"
 
-#define NJSON_SERIALIZE_INIT _NJSON_SERIALIZE_INIT(njson_val)
-#define NJSON_DESERIALIZE_INIT _NJSON_DESERIALIZE_INIT(_njson_tmp_val_)
+#define NJSON_VAL njson_val
+#define NJSON_VAR njson_var
+#define NJSON_TMP_VAL _njson_tmp_val_
 
-#define NJSON_SET(M) _NJSON_SET(njson_val,#M,M)
-#define NJSON_SET_AS_TYPE(M, T) _NJSON_SET(njson_val,#M,(T)(M))
-#define NJSON_GET(M) _NJSON_GET(njson_val,#M,&(M),_njson_tmp_val_)
-#define NJSON_GET_AS_TYPE(M, T) _NJSON_GET(njson_val,#M,(T *)(&(M)))
+#define NJSON_SERIALIZE_INIT _NJSON_SERIALIZE_INIT(NJSON_VAL)
+#define NJSON_DESERIALIZE_INIT _NJSON_DESERIALIZE_INIT(NJSON_TMP_VAL)
+#define NJSON_RET return njson_val
 
-#define NJSON_SET_IF_KEY(M) if(strcmp(#M,_key_) == 0) { NJSON_SET(M); } else
-#define NJSON_SERIALIZE_SUPER_CLASS(S) _NJSON_SERIALIZE_SUPER_CLASS((S *)this,njson_val)
-#define NJSON_DESERIALIZE_SUPER_CLASS(S) _NJSON_DESERIALIZE_SUPER_CLASS((S *)this,njson_val)
+#define NJSON_SET(M) _NJSON_SET(NJSON_VAL,#M,M)
+#define NJSON_GET(M) _NJSON_GET(NJSON_VAL,#M,&(M),NJSON_TMP_VAL)
+#define NJSON_SET_AS_TYPE(M, T) _NJSON_SET(NJSON_VAL,#M,(T)(M))
+#define NJSON_GET_AS_TYPE(M, T) _NJSON_GET(NJSON_VAL,#M,(T *)(&(M)))
 
-#define SET_IF(K, M) _NJSON_SET_IF_NOT_DEFAULT(_root_obj_,K,M)
+#define NJSON_SERIALIZE_SUPER_CLASS(S) _NJSON_SERIALIZE_SUPER_CLASS((S *)this,NJSON_VAL)
+#define NJSON_DESERIALIZE_SUPER_CLASS(S) _NJSON_DESERIALIZE_SUPER_CLASS((S *)this,NJSON_VAL)
 
-#define NJSON_NSET(M) _NJSON_SET(njson_val,#M,njson_var.get_##M())
-#define NJSON_NGET(M) _NJSON_GET(njson_val,#M,&(njson_var->get_##M()),_njson_tmp_val_)
-#define NJSON_NSERIALIZE_SUPER_CLASS(S) _NJSON_SERIALIZE_SUPER_CLASS((S *)&njson_var,njson_val)
-#define NJSON_NDESERIALIZE_SUPER_CLASS(S) _NJSON_DESERIALIZE_SUPER_CLASS((S *)njson_var,njson_val)
+#define NJSON_NSET(M) _NJSON_SET(NJSON_VAL,#M,NJSON_VAR.get_##M())
+#define NJSON_NGET(M) _NJSON_GET(NJSON_VAL,#M,&(NJSON_VAR->get_##M()),NJSON_TMP_VAL)
+#define NJSON_NSERIALIZE_SUPER_CLASS(S) _NJSON_SERIALIZE_SUPER_CLASS((S *)&NJSON_VAR,NJSON_VAL)
+#define NJSON_NDESERIALIZE_SUPER_CLASS(S) _NJSON_DESERIALIZE_SUPER_CLASS((S *)NJSON_VAR,NJSON_VAL)
 
 inline int cut_string(char *cstr, char c);
 
